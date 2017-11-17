2dafe71f929451dbb24bf5675950b6bc2d747e0c
Amanda Kirchner <geeky.brewster@gmail.com>
2017-11-17 12:24:52 -0600

    Added DB for pre-built responses, which now append to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
   //Initialize textarea counter
   $('input#input_text, textarea#message').characterCounter();
 
+  //Load pre-built Responses
+  getResponses();
+
   //Start of Code from Code Pen
   $(function(){
   if ('speechSynthesis' in window) {
@@ -62,3 +65,35 @@ $(document).ready(function() {
 
 
 }); //end of doc.ready
+
+function getResponses(){
+  console.log( 'in getResponses' );
+  $('#viewResponses').empty();
+  // ajax call to server to get responses
+  $.ajax({
+    url: '/responses',
+    type: 'GET',
+    success: function( response ){
+      console.log( 'got some response: ', response );
+      var responses = response.responses;
+      for(var i = 0; i < responses.length; i++){
+        var text = responses[i];
+        var $div1 = $('<div class="col s6 m4"></div>');
+        var $div2 = $('<div class="card blue-grey darken-1"></div>');
+        var $div3 = $('<div class="card-content white-text" data-keyword="' + text.response_id +'"></div>');
+        $div3.append('<span class="card-title">' + text.response_text + '</span>');
+
+        var $div4 = $('<div class="card-action"><div>');
+        $div4.append('<button class="say" class="waves-effect waves-light btn" data-keyword="' + text.response_id + '">Speak</button>');
+        $div4.append('<button class="delete" class="waves-effect waves-light btn" data-id="' + text.id +'">Delete</button>');
+
+        $div2.append($div3);
+        $div2.append($div4);
+        $div1.append($div2);
+        $('#viewResponses').append($div1);
+
+      }
+    } // end success
+  }); //end ajax
+  // display on DOM with buttons that allow edit of each
+} // end getResponses
--- server/server.js
+++ server/server.js
@@ -6,6 +6,43 @@ var port = process.env.PORT || 7000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+var pg = require('pg');
+
+var config = {
+  database: 'side_projects', // name of your database
+  host: 'localhost', // where is your database?
+  port: 5432, // port for the database
+  max: 10, // how many connections at one time?
+  idleTimeoutMillis: 30000 // 30 second time out
+};
+
+var pool = new pg.Pool(config);
+
+// Sending responses DB data to client from server
+app.get('/responses', function(req, res){
+  pool.connect(function(errorConnectingToDatabase, db, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.');
+      res.sendStatus(500);
+    } else {
+      var queryText = 'SELECT * FROM "responses";';
+      db.query(queryText, function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Attempted to query with', queryText);
+          console.log('Error making query');
+          res.sendStatus(500);
+        } else {
+          console.log(queryText);
+          res.send({responses: result.rows});
+        }
+      }); // end query
+    } // end if
+  }); // end pool
+}); // end of GET
+
+
+
 // Serve back static files by default
 app.get('/*', function(req, res){
   var file = req.params[0] || 'views/index.html';
